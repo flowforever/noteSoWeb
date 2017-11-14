4ebffac6b9d5823253c3adf38430c1d6396dd305
trump.wang <trump.wang@ringcentral.com>
2017-11-14 23:08:40 +0800

    1. custom toolbar

--- src/note_app/pages/EditorPage.js
+++ src/note_app/pages/EditorPage.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactQuill from 'react-quill'; // ES6
 import 'react-quill/dist/quill.snow.css'; // ES6
 
+const CustomButton = () => <span className="octicon octicon-star"/>
+function insertStar () {
+    const cursorPosition = this.quill.getSelection().index
+    this.quill.insertText(cursorPosition, "★")
+    this.quill.setSelection(cursorPosition + 1)
+}
+
+const CustomToolbar = () => (
+    <div id="toolbar">
+        <select className="ql-header">
+            <option></option>
+            <option value="1"></option>
+            <option value="2"></option>
+        </select>
+        <button className="ql-bold"></button>
+        <button className="ql-italic"></button>
+        <button className="ql-underline"></button>
+        <button className="ql-strike"></button>
+        <button className="ql-blockquote"></button>
+    </div>
+);
+
+
+
 export default class EditorPage extends React.Component {
 
     state = {
@@ -9,16 +33,26 @@ export default class EditorPage extends React.Component {
     };
 
     render() {
-        return <ReactQuill
-            theme={'snow'}
-            value={this.state.text}
-            onChange={this.onEdit}
-            style={{width:'100%', height: '100%', position: 'fixed'}}
-            modules={this.modules}
-            formats={this.formats}
-        />;
+        return [
+            <CustomToolbar key={'toolbar'}/>,
+            <ReactQuill
+                key={'editor'}
+                theme={'snow'}
+                value={this.state.text}
+                onChange={this.onEdit}
+                style={{width: '100%', height: '100%', position: 'fixed'}}
+                modules={{
+                    toolbar: {
+                        container: "#toolbar",
+                        handlers: {
+                            "insertStar": insertStar,
+                        }
+                    }
+                }}
+                formats={this.formats}
+            />
+        ]
     }
-
 
     formats = [
         'header', 'font', 'size',
@@ -29,7 +63,7 @@ export default class EditorPage extends React.Component {
 
     modules = {
         toolbar: [
-            [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+            [{'header': '1'}, {'header': '2'}, {'font': []}],
             [{size: []}],
             ['bold', 'italic', 'underline', 'strike', 'blockquote'],
             [{'list': 'ordered'}, {'list': 'bullet'},
